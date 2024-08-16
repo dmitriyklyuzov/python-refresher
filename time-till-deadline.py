035d5b57401c4f19aa586acbb28f7c2871e7b8d2
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2024-08-16 08:00:15 -0400

    cleaned up the imports

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime
 
 user_input = input("enter your goal with a deadline (MM.DD.YYYY) separated by colon: ")
 goal, deadline = user_input.split(":")
 
-deadline_date = datetime.datetime.strptime(deadline, "%m.%d.%Y")
+deadline_date = datetime.strptime(deadline, "%m.%d.%Y")
 
 # calculate how many days are left until the deadline
-todays_date = datetime.datetime.today()
+todays_date = datetime.today()
 time_left = deadline_date - todays_date
 print(f"You have {time_left.days} days left to {goal}.")
